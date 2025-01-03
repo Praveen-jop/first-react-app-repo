330fefc113cc9956bd248b19a5c1ad517f6ea0ef
PRAVEEN-JO <115061255+Praveen-jop@users.noreply.github.com>
2025-01-03 17:23:16 +0530

    update
    made update on 03/01/25

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './components/Greeting.jsx'
+import Test from './components/test.jsx'
+import Render_app from './components/Render_app.jsx'
+import Conditional_render from './components/Conditional_render.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <Conditional_render />
   </StrictMode>,
 )
